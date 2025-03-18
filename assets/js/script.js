2ae60859ff5698b1f3c77e4eb98a1366ecf63d11
ixxmmivsdrgn <longtruongphamhai@gmail.com>
2025-03-18 11:39:03 +0700

    12PM 18-03-25

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// LOAD HEADER & FOOTER
+
 document.addEventListener("DOMContentLoaded", function () {
   fetch("components/header.html")
     .then((response) => response.text())
@@ -11,6 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
       (data) => (document.getElementById("footer-placeholder").innerHTML = data)
     );
 });
+
+// ACTIVE TAB
 
 document.addEventListener("DOMContentLoaded", function () {
   // Load header trước
@@ -30,7 +34,7 @@ function setActiveLink() {
       currentPath = "index.html";
   }
 
-  console.log("Current path:", currentPath);
+  // console.log("Current path:", currentPath);
 
   // Duyệt qua tất cả các thẻ <a> trong nav
   document.querySelectorAll("nav a").forEach(link => {
@@ -39,4 +43,78 @@ function setActiveLink() {
           link.classList.add("active");
       }
   });
+}
+
+// PROJECTS
+
+function openProject1()
+{
+  document.getElementById("none-project").style.display = "none";
+  document.getElementById("project-demo").style.display = "block";
+  document.getElementById("image-project1").style.display = "block";
+  document.getElementById("link-project1").style.display = "block";
+  document.getElementById("image-project2").style.display = "none";
+  document.getElementById("link-project2").style.display = "none";
+  document.getElementById("image-project3").style.display = "none";
+  document.getElementById("link-project3").style.display = "none";
+}
+
+function openProject2()
+{
+  document.getElementById("none-project").style.display = "none";
+  document.getElementById("project-demo").style.display = "block";
+  document.getElementById("image-project1").style.display = "none";
+  document.getElementById("link-project1").style.display = "none";
+  document.getElementById("image-project2").style.display = "block";
+  document.getElementById("link-project2").style.display = "block";
+  document.getElementById("image-project3").style.display = "none";
+  document.getElementById("link-project3").style.display = "none";
+}
+
+function openProject3()
+{
+  document.getElementById("none-project").style.display = "none";
+  document.getElementById("project-demo").style.display = "block";
+  document.getElementById("image-project1").style.display = "none";
+  document.getElementById("link-project1").style.display = "none";
+  document.getElementById("image-project2").style.display = "none";
+  document.getElementById("link-project2").style.display = "none";
+  document.getElementById("image-project3").style.display = "block";
+  document.getElementById("link-project3").style.display = "block";
+}
+
+function closeProject()
+{
+  document.getElementById("none-project").style.display = "block";
+  document.getElementById("project-demo").style.display = "none";
+  document.getElementById("image-project1").style.display = "none";
+  document.getElementById("link-project1").style.display = "none";
+  document.getElementById("image-project2").style.display = "none";
+  document.getElementById("link-project2").style.display = "none";
+  document.getElementById("image-project3").style.display = "none";
+  document.getElementById("link-project3").style.display = "none";
+}
+
+// CONTACT
+
+function showPhoneNum()
+{
+  alert("Phone number: +84 862 091 004");
+}
+
+function resetButton()
+{
+  document.getElementById("email-input").value = "";
+  document.getElementById("title-input").value = "";
+  document.getElementById("content-input").value = "";
+}
+
+function sendButton()
+{
+  let emailValue = document.getElementById("email-input").value;
+  let titleValue = document.getElementById("title-input").value;
+  let contentValue = document.getElementById("content-input").value;
+  if(emailValue == "")  alert("Email not entered! Please enter your email.")
+  let contactForm = [emailValue, titleValue, contentValue];
+  console.log(contactForm);
 }
